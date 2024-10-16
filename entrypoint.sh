2e29c7f5906c0f55beb8f3ae7e400937d27b765a
Cavin <166345236+cavinwela@users.noreply.github.com>
2024-10-17 00:29:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,12 @@ if [ $pull_request_id == "null" ]; then
   exit 1
 fi
 
-cd /github/workspace
+maintainer=${GITHUB_REPOSITORY%/*}
+eval git clone "https://${maintainer}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${GITHUB_REPOSITORY}
+cd $GITHUB_REPOSITORY
+eval git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+eval git fetch origin pull/$pull_request_id/head:$branch_name
+eval git checkout $branch_name
 
 latest_head=$(git rev-parse HEAD)
 
